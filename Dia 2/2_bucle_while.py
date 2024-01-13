f6bbcf1d482b2c12cef283258b5e32dad021ef5c
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2024-01-12 23:10:02 -0500

    finalizacion semana01

--- Dia 2/2_bucle_while.py
+++ Dia 2/2_bucle_while.py
@@ -14,8 +14,22 @@ while numero < 20:
 # print(valor)
 
 # Adivina el numero
-numero = 75
+numero = 225
 numero_adivinado = 0
 while numero_adivinado != numero:
     # todos los valores que le pasemos al input se capturaran como string
     numero_adivinado = int(input('Por favor ingresa un numero:'))
+    # si me ingresa 20 entonces decirle que el numero es mayor
+    # si me ingresa 250 entonces decirle que el numero es menor
+    if numero_adivinado < numero:
+        print('El numero es Mayor!')
+    elif numero_adivinado == numero:
+        print('Felicidades Acertaste!')
+    else:
+        print('El numero es Menor!')
+
+
+while True:
+    print('hola soy infinito')
+    # para salir de un bucle ya sea for o while
+    break
