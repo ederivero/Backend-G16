a473706853995e88cb1ee4639462452375cec5e2
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2024-01-24 23:07:00 -0500

    creacion de modelos

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from variables import conexion
+from models.usuario import UsuarioModel
+from models.direccion import DireccionModel
 
 app = Flask(__name__)
 # print(app.config)
@@ -11,6 +13,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/alumno
 # Inicializar la conexion a nuestra BD
 # al momento de pasarle la aplicacion de flask en esta se encontraras la cadena de conexion a la bd
 conexion.init_app(app)
+
+
+# before_request > se mandara a llamar a esta funcionabilidad antes de cualquier request (peticion)
+@app.before_request
+def inicializacion():
+    # create_all > crea todas las tablas que no se han creado en la base de datos
+    conexion.create_all()
+
+
+@app.route('/')
+def inicial():
+    return {
+        'message': 'Bienvenido a mi API de usuarios'
+    }
 
 
 if __name__ == '__main__':
---
+++ models/usuario.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, types
+# Si queremos utilizar un tipo de dato en especial para un determinado motor de base de datos
+# from sqlalchemy.dialects.postgresql.types import MONEY
+from variables import conexion
+
+
+# Para indicar que esta clase sera una tabla en la base de datos utilizamos la conexion a la base de datos
+class UsuarioModel(conexion.Model):
+    id = Column(name='id',
+                type_=types.Integer,
+                autoincrement=True,
+                primary_key=True)
+    # Si no colocamos el parametro name entonces el nombre de la columna sera el mismo que el nombre del atributo
+    # nullable > puede admitir valores nulos o no, su valor por defecto es True
+    nombre = Column(type_=types.String(100), nullable=False)
+    apellido = Column(type_=types.String(100))
+    fechaNacimiento = Column(name='fecha_nacimiento', type_=types.Date)
+    correo = Column(type_=types.String(100), unique=True)
+
+    # Ahora para indicar como queremos que se llame esta tabla en la bd
+    __tablename__ = 'usuarios'
