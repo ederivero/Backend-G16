fcbf330da199aa691ef81d3e3c2c8bc210d21eaf
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2024-03-15 21:11:24 -0500

    uso de socket con bd y comandos mongo

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,10 @@
-const socket = io('https://eb84-2800-200-f408-c085-e20e-7d41-ab2e-ad97.ngrok-free.app')
+const BACKEND_URL = 'http://127.0.0.1:3000'
+const socket = io(BACKEND_URL)
 
 const container = document.getElementById('container')
 const nombre = document.getElementById('nombre')
 const registrarUsuario = document.getElementById('btnRegistro')
+const registroContainer = document.getElementById('registro')
 
 container.classList.add('oculto')
 const mensaje = document.getElementById('mensaje')
@@ -15,6 +17,30 @@ socket.on('connect', () => {
     console.log(id)
 })
 
+registrarUsuario.addEventListener('click', async (e) => {
+    e.preventDefault()
+
+    const resultado = await fetch(`${BACKEND_URL}/crear-usuario`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            socketId: id,
+            nombre: nombre.value
+        })
+    })
+
+    if (resultado.status === 201) {
+        const { message } = await resultado.json()
+        alert(message)
+        // hacemos visible a nuestro contenedor de mensajes
+        container.classList.remove('oculto')
+        // removemos nuestro contenedor del login
+        registroContainer.remove()
+    } else {
+        alert('Error al crear el usuario')
+    }
+
+})
 // Agregamos el evento cuando se presione Enter al input
 mensaje.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
@@ -41,4 +67,22 @@ socket.on('mensajeRecibido', (data) => {
 
     mensajes.appendChild(div)
 
+})
+
+socket.on('historial-mensajes', (data) => {
+    console.log(data)
+    for (const mensaje of data.mensajes) {
+        console.log(mensaje)
+        const div = document.createElement('div')
+        const parrafo = document.createElement('p')
+        parrafo.innerHTML = `${mensaje.usuario.nombre} dice: ${mensaje.texto}`
+        // Agregar una clase a nuestro parrafo
+        parrafo.className = 'burbuja-texto'
+        // Dependiendo si es el mismo usuario que esta enviando el mensaje entonces lo colocaremos a la izquierda, caso contrario a la derecha
+        div.className = 'izquierda'
+
+        div.appendChild(parrafo)
+
+        mensajes.appendChild(div)
+    }
 })
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { Server } from 'socket.io'
 // Libreria que sirve para crear un servidor de la forma mas basica sin usar express u otros es lo que utiliza socket.io para poder funcionar
 import { createServer } from 'http'
 import { PrismaClient } from '@prisma/client'
-
+import cors from 'cors'
 
 const conector = new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL3 } } })
 
@@ -14,9 +14,15 @@ const servidorHTTP = createServer(servidor)
 // Los cors configurados en nuestro servidor pueden tener diferente configuracion que los del socket
 const servidorSocket = new Server(servidorHTTP, { cors: { origin: '*' } })
 
+servidor.use(express.json())
+// Estos cors no son los mismos que los cors declarados en el socket, ya que son diferentes tipos de comunicacion con mi backend uno es x sockets y el otro por http request
+servidor.use(cors({ allowedHeaders: ['Content-Type', 'Authorization', 'accepts'], methods: ['POST', 'GET'], origin: '*' }))
+
+
 servidor.post('/crear-usuario', async (req, res) => {
     const { socketId, nombre } = req.body
 
+    console.log(req.body)
     await conector.usuario.create({ data: { socketId, nombre } })
 
     return res.status(201).json({
@@ -24,12 +30,21 @@ servidor.post('/crear-usuario', async (req, res) => {
     })
 })
 
+const obtenerMensajes = async () => {
+    const mensajes = await conector.mensaje.findMany({ include: { usuario: true } })
+    console.log(mensajes)
+    return { mensajes }
+}
+
+
 // Cuando un usuario se quiera conectar a mi servidor de socket este lo hara por el metodo 'on' mediante el evento 'connection'
-servidorSocket.on('connection', (cliente) => {
+servidorSocket.on('connection', async (cliente) => {
     console.log(cliente.id)
 
     cliente.emit('saludo', { message: 'Bienvenido a mi Socket de NodeJs' })
 
+    // cuando el usuario se conecte al servidor de socket vamos a retornar todos los mensajes en la base de datos
+    cliente.emit('historial-mensajes', await obtenerMensajes())
     // ahora escucharemos un evento que nos enviara el cliente
     cliente.on('mensaje', async (texto) => {
         console.log(texto)
